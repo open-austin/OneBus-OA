9ac3359bef6a453f39900eb5ae0e31370aa0d9c2
Glenn Katzen <glenn.katzen@gmail.com>
2025-07-25 18:34:01 -0500

    Finish API response

--- api/views.py
+++ api/views.py
@@ -10,7 +10,7 @@ from api.poi_models import poi_getter
 # ImportError: attempted relative import beyond top-level package
 
 @csrf_exempt # Disable CSRF verification. Since we're not dealing with users or authentication yet, this should be safe.
-def test(request):
+def determine_stops_and_pois(request):
     if request.method != 'POST':
         return JsonResponse({'error': 'HTTP method not supported.'}, status=400)
     
@@ -19,8 +19,9 @@ def test(request):
         data = json.loads(request.body.decode('utf-8'))
         latitude_input = data.get('latitude')
         longitude_input = data.get('longitude')
-    except json.JSONDecodeError:
-        return JsonResponse({'error': 'Invalid JSON data'}, status=400)
+    except json.JSONDecodeError as e:
+        print(f"JSON decoding error: {e}")
+        return JsonResponse({'error': f'Invalid JSON data'}, status=400)
 
     if latitude_input is None or longitude_input is None:
         return JsonResponse({'error': 'Latitude and longitude values are required.'}, status=400)
@@ -28,8 +29,9 @@ def test(request):
     try:
         latitude = float(latitude_input)
         longitude = float(longitude_input)
-    except ValueError:
-        return JsonResponse({'error': 'Latitude and longitude must be valid numbers.'}, status=400)
+    except ValueError as e:
+        print(f"Coordinate conversion error: {e}")
+        return JsonResponse({'error': f'Latitude and longitude must be valid numbers'}, status=400)
     
     data = get_data()
 
@@ -47,36 +49,27 @@ def test(request):
         "stops": [],
         "pois": [],
     }
-    for index, row in all_stops.iterrows():        
-        bus_stop = {
+    for _, row in all_stops.iterrows():        
+        stop = {
             'latitude': row['stop_lat'],
             'longitude': row['stop_lon'],
             'stop_name': row['stop_name'],
             'headsign': row['trip_headsign'],
         }
 
-        response['stops'].append(bus_stop)
+        response['stops'].append(stop)
 
-    # TODO: Add POI data to response
-    # for index, row in poi_df.iterrows():
-    #     poi_lat = row.geometry.y
-    #     poi_lon = row.geometry.x
-    #     poi_busstop = str(row['stop_name'])
-    #     poi_bus = row['route_id']
-    #     poi_name = row['name']
-    #     first_stop_number = poi_df[(poi_df['origin stop'] == True) & (poi_df['route_id'] == poi_bus)]['stop_sequence']
-    #     # print(first_stop_number)
-    #     # num_stops = row['stop_sequence'] - row['first_stop_number']
-    #     poi_amenity = row['amenity']
-    #     icon_name = row['icon']
-    #     icon_color = row['color']
-        
-    #     popup_text = folium.Html(f"Take bus {poi_bus} for stops <br> Closest bus stop: {poi_busstop}.<br>Name of POI: {poi_name}.<br>Type of POI: {poi_amenity}.<br>.", script = True)
-
-    #     # Add a marker for each row to the map
-    #     folium.Marker(
-    #         location = [poi_lat, poi_lon], 
-    #         popup=folium.Popup(popup_text, parse_html=True, max_width=300),
-    #         icon=folium.Icon(color=icon_color ,icon=icon_name, prefix='fa')).add_to(map)
+    for _, row in poi_df.iterrows():
+        poi = {
+            'latitude': row.geometry.y,
+            'longitude': row.geometry.x,
+            'stop_name': row['stop_name'],
+            'route_id': row['route_id'],
+            'name': row['name'],
+            'amenity': row['amenity'],
+            'icon': row['icon'],
+            'color': row['color'],
+        }
+        response['pois'].append(poi)
 
     return JsonResponse(response)
