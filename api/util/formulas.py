bb6a7e23dcd630429163b3a1d37343e628e68d6b
mmaliu97 <martincodesthings@gmail.com>
2025-09-30 11:50:15 -0500

    remove old haversine formula

--- api/util/formulas.py
+++ api/util/formulas.py
@@ -12,11 +12,3 @@ def haversine(lat1, lon1, lat2, lon2):
     distance = 6371 * c * 1000 # Earth radius in km, convert to meters
     return distance
 
-def haversine_busstop(lat1, lon1, lat2, lon2):
-    R = 6371  # Radius of the E arth in kilometers
-    dlat = radians(lat2 - lat1)
-    dlon = radians(lon2 - lon1)
-    a = sin(dlat / 2) ** 2 + cos(radians(lat1)) * cos(radians(lat2)) * sin(dlon / 2) ** 2
-    c = 2 * atan2(sqrt(a), sqrt(1 - a))
-    distance = R * c
-    return distance
